823f55694662dc45f6718e9b5cba03ca8d2108db
Jimmy Chion <ajimmykid@gmail.com>
2018-07-06 09:58:17 -0700

    typo

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,6 +2,6 @@ import CMS from 'netlify-cms'
 
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 
-CMS.registerPreviewStyle('../layouts/styles.css')
+CMS.registerPreviewStyle('../layouts/style.css')
 CMS.registerPreviewStyle('../layouts/template-styles/blog-post-style-1.css')
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
