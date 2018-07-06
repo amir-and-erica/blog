5db3416be24ba452a6e063581588c898eb32935c
Jimmy Chion <ajimmykid@gmail.com>
2018-07-06 12:22:46 -0700

    figured this shit out

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,5 +2,5 @@ import CMS from 'netlify-cms'
 
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 
-CMS.registerPreviewStyle('/style.css')
+CMS.registerPreviewStyle('/styles.css') //-- path is absolute from built project
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
