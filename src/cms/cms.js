82d0fe18a4f0d8d2b143ce7d197cc3bf7dcb62ae
Jimmy Chion <ajimmykid@gmail.com>
2018-07-06 10:04:11 -0700

    trying ot get preview to work

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,5 +3,4 @@ import CMS from 'netlify-cms'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 
 CMS.registerPreviewStyle('../layouts/style.css')
-CMS.registerPreviewStyle('../layouts/template-styles/blog-post-style-1.css')
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
