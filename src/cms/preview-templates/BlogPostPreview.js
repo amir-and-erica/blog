e462aca1d24b754d4f388fae0c6dd19ef411cd2a
Jimmy Chion <ajimmykid@gmail.com>
2018-07-13 16:08:49 -0700

    don't know how these templates work

--- src/cms/preview-templates/BlogPostPreview.js
+++ src/cms/preview-templates/BlogPostPreview.js
@@ -6,7 +6,6 @@ const BlogPostPreview = ({ entry, widgetFor }) => (
   <BlogPostTemplate
     content={widgetFor('body')}
     description={entry.getIn(['data', 'description'])}
-    frontmatter={entry.getIn(['data', 'frontmatter'])}
   />
 )
 
--- src/pages/tags/index.js
+++ src/pages/tags/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Head from '../../layout/head'
 import Link from 'gatsby-link'
 import styled from 'styled-components'
+import TemplateWrapper from '../../layout/index'
 import {TagsContainer, TagLink, TagTitle} from '../../components/TagsSection'
 import {kebabCase} from 'lodash'
 
@@ -17,23 +18,25 @@ const Container = styled.div`
 const TagsPage = ({
   data: { allMarkdownRemark: { group }, site: { siteMetadata: { title } } },
 }) => (
-  <Container >
-    <Head
-      url={`https://blog.bythebay.cool/tags/`}
-      title={`Tags | ${title}`}
-      headline={`${title} posts by tags`}
-      description="A collection of posts"
-    />
+  <TemplateWrapper>
+    <Container >
+      <Head
+        url={`https://blog.bythebay.cool/tags/`}
+        title={`Tags | ${title}`}
+        headline={`${title} posts by tags`}
+        description="A collection of posts"
+      />
 
-    <h1>Browse by topic</h1>
-    <TagsContainer >
-      {group.map(tag => (
-        <TagLink key={tag.fieldValue} to={`/tags/${kebabCase(tag.fieldValue)}/`}>
-          <TagTitle>{tag.fieldValue} ({tag.totalCount})</TagTitle>
-        </TagLink>
-      ))}
-    </TagsContainer>
-  </Container>
+      <h1>Browse by topic</h1>
+      <TagsContainer >
+        {group.map(tag => (
+          <TagLink key={tag.fieldValue} to={`/tags/${kebabCase(tag.fieldValue)}/`}>
+            <TagTitle>{tag.fieldValue} ({tag.totalCount})</TagTitle>
+          </TagLink>
+        ))}
+      </TagsContainer>
+    </Container>
+  </TemplateWrapper>
 )
 
 export default TagsPage
